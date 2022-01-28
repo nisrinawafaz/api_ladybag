36b5dae254971ee6f23e8691f49041000a3c2a68
nisrinawafaz <89191583+nisrinawafaz@users.noreply.github.com>
2022-01-28 22:33:19 +0700

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 
   Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://249d-114-142-172-35.ngrok.io'
+      origins 'https://ladybag.herokuapp.com'
       resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :options, :delete], :credentials => true
     end
     end
